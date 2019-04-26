7aba2e82dbf84725b7bbc9e524d102cde9656b29
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-04-26 12:37:19 -0700

    styling fixed

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -7,11 +7,11 @@ const Character = props => {
     <div className="charSheet">
       <div className="list">
         <h3>{props.charProp.name}</h3>
-        <div>Gender: {props.charProp.gender}</div>
-        <div>Height: {props.charProp.height}</div>
-        <div>Mass: {props.charProp.mass}</div>
-        <div>Hair Color: {props.charProp.hair_color}</div>
-        <div>Eye Color: {props.charProp.eye_color}</div>
+        <div className="list-content">Gender: {props.charProp.gender}</div>
+        <div className="list-content">Height: {props.charProp.height}</div>
+        <div className="list-content">Mass: {props.charProp.mass}</div>
+        <div className="list-content">Hair Color: {props.charProp.hair_color}</div>
+        <div className="list-content">Eye Color: {props.charProp.eye_color}</div>
       </div>
     </div>
   );
